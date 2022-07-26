4e2dc22747cc8f5d35b2be156c9aed744bde99a1
lambertkevin <kevin.lambert@ledger.fr>
2022-07-26 16:43:08 +0200

    Add EVM deviceTransactionConfig

---
+++ libs/ledger-live-common/src/families/evm/deviceTransactionConfig.ts
@@ -0,0 +1,51 @@
+import { DeviceTransactionField } from "../../transaction";
+import { Account, TransactionStatus } from "../../types";
+import { Transaction as EvmTransaction } from "./types";
+
+function getDeviceTransactionConfig({
+  account,
+  transaction,
+}: {
+  account: Account;
+  transaction: EvmTransaction;
+  status: TransactionStatus;
+}): Array<DeviceTransactionField> {
+  const { mode } = transaction;
+  const fields: {
+    type: string;
+    label: string;
+    value?: string;
+    address?: string;
+  }[] = [];
+
+  switch (mode) {
+    case "send":
+      fields.push({
+        type: "amount",
+        label: "Amount",
+      });
+      fields.push({
+        type: "address",
+        label: "Address",
+        address: transaction.recipient,
+      });
+      fields.push({
+        type: "text",
+        label: "Network",
+        value: account.currency.name.replace("Lite", "").trim(),
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  fields.push({
+    type: "fees",
+    label: "Max fees",
+  });
+
+  return fields as Array<DeviceTransactionField>;
+}
+
+export default getDeviceTransactionConfig;
